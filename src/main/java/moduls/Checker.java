7e6677559ee51b4f43e36b846ac07422aaf297a4
Xaabibas <tumuyhabirov@yandex.ru>
2025-10-14 12:23:10 +0300

    another one fix Checker

--- src/main/java/moduls/Checker.java
+++ src/main/java/moduls/Checker.java
@@ -20,13 +20,13 @@ public class Checker {
     }
 
     public boolean checkBox(float x, float y, float r) {
-        if (x >= 0 && y >= 0) {
-            return x < r / 2 && y < r;
+        if (x >= 0 && y > 0) {
+            return x <= r / 2 && y <= r;
         }
-        if (x >= 0 && y < 0) {
+        if (x >= 0 && y <= 0) {
             return y >= x / 2 - r / 2;
         }
-        if (x < 0 && y < 0) {
+        if (x < 0 && y <= 0) {
             return x*x + y*y <= r*r / 4;
         }
         return false;
